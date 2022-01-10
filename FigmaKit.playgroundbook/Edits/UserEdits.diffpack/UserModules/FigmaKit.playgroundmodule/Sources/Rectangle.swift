6eebcae3cfaf8f4bb4287710fae6fb74cc5ece75
Jeff Verkoeyen <jverkoey@gmail.com>
2022-01-10 00:12:17 -0500

    Add a Rectangle type.

---
+++ FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKit.playgroundmodule/Sources/Rectangle.swift
@@ -0,0 +1,13 @@
+/// A Figma rectangle type.
+/// "A rectangle that expresses a bounding box in absolute coordinates".
+/// https://www.figma.com/developers/api#rectangle-type
+public struct Rectangle: Codable {
+    /// The x coordinate of top left corner of the rectangle.
+    public let x: Double
+    /// The y coordinate of top left corner of the rectangle.
+    public let y: Double
+    /// The width of the rectangle.
+    public let width: Double
+    /// The height of the rectangle.
+    public let height: Double
+}
---
+++ FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKitTests.playgroundmodule/Sources/RectangleTests.swift
@@ -0,0 +1,18 @@
+import FigmaKit
+import Foundation
+import XCTest
+
+public final class RectangleTests: XCTestCase {
+    public func testDecoding() throws {
+        let json = """
+            {"x": 1, "y": 2, "width": 3, "height": 4}
+            """.data(using: .utf8)!
+        let decoder = JSONDecoder()
+        let rectangle = try decoder.decode(Rectangle.self, from: json)
+        
+        XCTAssertEqual(rectangle.x, 1)
+        XCTAssertEqual(rectangle.y, 2)
+        XCTAssertEqual(rectangle.width, 3)
+        XCTAssertEqual(rectangle.height, 4)
+    }
+}
