84ff076a76250504dddf5ccc6beaa479a17492e2
Jeff Verkoeyen <jverkoey@gmail.com>
2022-01-09 23:39:48 -0500

    Remove unnecessary init

--- FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKitTests.playgroundmodule/Sources/FigmaKitTests.swift
+++ FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKitTests.playgroundmodule/Sources/FigmaKitTests.swift
@@ -3,10 +3,6 @@ import FigmaKit
 import XCTest
 
 public final class FigmaKitTests: XCTestCase {
-    public convenience init() {
-        self.init(name: "") { _ in }
-    }
-    
     public func testTrue() {
         XCTAssertTrue(true)
     }
--- FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/XCTest.playgroundmodule/Sources/XCTest.swift
+++ FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/XCTest.playgroundmodule/Sources/XCTest.swift
@@ -1,6 +1,6 @@
 public typealias XCTestCaseClosure = (XCTestCase) throws -> Void
 
 open class XCTestCase {
-    public required init(name: String, testClosure: @escaping XCTestCaseClosure) {
+    public init() {
     }
 }
