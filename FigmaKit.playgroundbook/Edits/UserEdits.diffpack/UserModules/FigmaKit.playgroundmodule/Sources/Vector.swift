346a180d2d420dc607ec311e7ce3d9b1a0efa062
Jeff Verkoeyen <jverkoey@gmail.com>
2022-01-12 23:20:59 -0500

    Handle empty vectors

--- FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKit.playgroundmodule/Sources/Vector.swift
+++ FigmaKit.playgroundbook/Edits/UserEdits.diffpack/UserModules/FigmaKit.playgroundmodule/Sources/Vector.swift
@@ -6,4 +6,11 @@ public struct Vector: Codable {
     public let x: Double
     /// The y coordinate of the vector.
     public let y: Double
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Self.CodingKeys)
+        
+        self.x = try container.decodeIfPresent(Double.self, forKey: .x) ?? 0
+        self.y = try container.decodeIfPresent(Double.self, forKey: .y) ?? 0
+    }
 }
